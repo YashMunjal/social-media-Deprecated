3f477a783caad5aae9c9f9ecf6ed8764b88d8362
yashmunjal2 <91688280+yashmunjal2@users.noreply.github.com>
2021-10-27 23:25:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 // Serve static assets in production
+//better to be dockerized
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
